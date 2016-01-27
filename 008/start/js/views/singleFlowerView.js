5ca85268924588776cd6b9df00a3b01e09d90c29
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-01-27 09:28:15 +0530

    All Changes
    All Changes with snippet

---
+++ 008/start/js/views/singleFlowerView.js
@@ -0,0 +1,12 @@
+//Namespace our flowerApp
+
+var app = app || {};
+
+// The view for a single model view, which is one flower
+
+app.singleFlowerView = Backbone.View.extend({
+tagName : "article",
+className : "flowerListItem",
+
+
+});
