bd4385a33e8095fb29c89bb77c580eb0a6722a64
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2018-11-27 11:45:03 +0530

    Removed potential security vulnerabilities in your dependencies

--- 01 The Anatomy of Backbonejs - I/00 Exercise Files/anatomyofbackbone-master/app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require backbone-rails
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require router
-
-$(function(){ TodoApp.start() });
--- 01 The Anatomy of Backbonejs - I/00 Exercise Files/anatomyofbackbone-master/config/routes.rb
+++
@@ -1,4 +0,0 @@
-AnatomyOfBackbone::Application.routes.draw do
-  resources :todos
-  root to: 'home#index'
-end
